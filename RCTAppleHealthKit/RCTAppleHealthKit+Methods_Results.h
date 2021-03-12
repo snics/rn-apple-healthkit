293e3199e6bbdad10b2d309b724590db0c8c68d6
Nico Swiatecki <nico@devninjas.io>
2021-03-12 17:10:10 +0100

    ✨ Add get carbohydrates

--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_Results.h
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Results.h
@@ -3,5 +3,6 @@
 @interface RCTAppleHealthKit (Methods_Results)
 
 - (void)results_getBloodGlucoseSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
+- (void)results_getCarbohydratesSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback;
 
 @end
--- RCTAppleHealthKit/RCTAppleHealthKit+Methods_Results.m
+++ RCTAppleHealthKit/RCTAppleHealthKit+Methods_Results.m
@@ -38,4 +38,37 @@
     }];
 }
 
+- (void)results_getCarbohydratesSamples:(NSDictionary *)input callback:(RCTResponseSenderBlock)callback
+{
+    HKQuantityType *carbohydratesType = [HKQuantityType quantityTypeForIdentifier:HKQuantityTypeIdentifierCarbohydrates];
+
+    HKUnit *mgPerdL = [HKUnit unitFromString:@"mg/dL"];
+
+    HKUnit *unit = [RCTAppleHealthKit hkUnitFromOptions:input key:@"unit" withDefault:mgPerdL];
+    NSUInteger limit = [RCTAppleHealthKit uintFromOptions:input key:@"limit" withDefault:HKObjectQueryNoLimit];
+    BOOL ascending = [RCTAppleHealthKit boolFromOptions:input key:@"ascending" withDefault:false];
+    NSDate *startDate = [RCTAppleHealthKit dateFromOptions:input key:@"startDate" withDefault:nil];
+    NSDate *endDate = [RCTAppleHealthKit dateFromOptions:input key:@"endDate" withDefault:[NSDate date]];
+    if(startDate == nil){
+        callback(@[RCTMakeError(@"startDate is required in options", nil, nil)]);
+        return;
+    }
+    NSPredicate * predicate = [RCTAppleHealthKit predicateForSamplesBetweenDates:startDate endDate:endDate];
+
+    [self fetchQuantitySamplesOfType:CarbohydratesType
+                                unit:unit
+                           predicate:predicate
+                           ascending:ascending
+                               limit:limit
+                          completion:^(NSArray *results, NSError *error) {
+        if(results){
+            callback(@[[NSNull null], results]);
+            return;
+        } else {
+            callback(@[RCTJSErrorFromNSError(error)]);
+            return;
+        }
+    }];
+}
+
 @end
